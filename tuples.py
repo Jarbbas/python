19de7b7fdd9775afeeee3bb0cdd869944860eb35
Emanuel <raulafonso88@gmail.com>
2020-05-03 23:00:48 +0100

    tupples or constants

---
+++ tuples.py
@@ -0,0 +1,7 @@
+#tuples são estrututras de dados que não podem ser alterados,
+#podem ser acedidos, mas não podemos alterar o seu conteudo
+#semelhante a listas, mas só usamos tuples quando queremos
+#valores que não serão alterados, tal como as constantes
+
+coordinates = (4, 5)
+print(coordinates[1])
