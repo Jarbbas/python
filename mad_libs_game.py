138b4f36f9ac29c8107a3082acbb039cfa0ab5c5
Emanuel <raulafonso88@gmail.com>
2020-05-03 22:59:41 +0100

    exemples of strings usage mad lib game

---
+++ mad_libs_game.py
@@ -0,0 +1,7 @@
+color = input("Enter a color: ")
+plural_noum = input("Enter a Plural noun: ")
+celebrity = input("Enter a celebrity name: ")
+
+print("Roses are " + color)
+print(plural_noum + " are blue ")
+print("and i love " + celebrity)
