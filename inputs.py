74962932bbf074e70f1a131a75a46cd20a9036c3
Emanuel <raulafonso88@gmail.com>
2020-05-03 23:00:14 +0100

    inputs in python

---
+++ inputs.py
@@ -0,0 +1,8 @@
+#input permite ao utilizador introduzir dados e
+#ao mesmo tempo podemos associar a uma variavel
+#para a podermos utilizar
+
+name = input("Enter your name: ")
+age = input("How old are you ?")
+
+print("Olá " + name + "! so you are " + age + " year's old")
