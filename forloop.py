3f8cd0c2da38874d4f45f65338cb469ccfe1f86a
Emanuel <raulafonso88@gmail.com>
2020-05-04 11:50:41 +0100

    ciclo for in python intro e exemples

---
+++ forloop.py
@@ -0,0 +1,24 @@
+#ciclos for no caso no puthon é diferente do que php ou javascript
+#este exemplo vai correr a sting e vai devolver cada letra da string
+#letter é uma função nativa do python
+
+for letter in "amor eu gosto de ti":
+    print(letter)
+
+#array friends
+family = ["Emanuel", "Carla", "Camilia"]
+#neste caso o ciclo for vai correr todos os elementos da array
+#podemos denominar qualquer nome para a variavel que vai correr o loop dentro da array
+for familymenber in family:
+    print(familymenber)
+
+#exemplo mais concencional onde o ciclo vai correr as 9 vezes e começando com o elemento 0
+for index in range(10):
+    print(index)
+
+#exemplo de um mais for onde segue o range e imprime quando passar pela 3º iteração
+for index in range(5):
+    if index == 3:
+        print("third iteration")
+    else:
+        print("Not")
--- guessing_game.py
+++ guessing_game.py
@@ -1,8 +1,16 @@
+#variaveis para validação
 my_secret = "python"
 guess = ""
 count = 0
 count_limit = 3
 game_over = False
+
+
+#condição while enquanto a variavel guess não for igual à palavra my_secret
+# e a condição game_over for True
+#vai incrementando as tentantivas no count e enquanto não exceder as 3 tentantivas
+#permite pedir mais uma tentativa, caso exceda as 3 tentativas sai do loop e valida
+#se ganhamos ou perdemos atraves do valor do game_over
 
 while guess != my_secret and not(game_over):
     if count < count_limit:
